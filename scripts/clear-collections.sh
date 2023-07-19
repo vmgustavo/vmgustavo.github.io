693dc75225d1784e1f67801e462a99b8b534a4ea
Gustavo Maia <vm.gustavo31@gmail.com>
2023-07-19 17:02:03 -0300

    update workflow

--- scripts/clear-collections.sh
+++ scripts/clear-collections.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/bash
 
+DIR=src/content
+if [ -d "$DIR" ];
+then
+    echo "$DIR directory exists."
+else
+	echo "$DIR directory does not exist."
+fi
+
 ! rm -r src/content/.obsidian
 ! rm -r src/content/uniques
 ! rm src/content/.gitignore
